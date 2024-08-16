df687440e679869233142203873b0bf6eeeea9af
pavlen5kiy <wolkpavel51@gmail.com>
2024-08-16 16:54:15 +0300

    Road skin shop added

--- main.py
+++ main.py
@@ -16,14 +16,17 @@ skins = {'red': ['open', 0],
          'blue': ['locked', 50],
          'brown': ['locked', 20],
          'green': ['locked', 250]}
-road_skins = ['road_basic', 'road_winter', 'road_desert']
+road_skins = {'road_basic': ['open', 0],
+              'road_winter': ['locked', 1500],
+              'road_desert': ['locked', 1000]}
 
 saving = {
     'coins': 0,
     'high_score': 0,
     'car_skin': 0,
     'road_skin': 0,
-    'car_skins': skins
+    'car_skins': skins,
+    'road_skins': road_skins
 }
 settings_file = {
     'music_volume': 1,
@@ -36,13 +39,14 @@ def close(*args):
     global settings_file
     global saving
 
-    song, coins, main_skin, road_skin, skins = args
+    song, coins, main_skin, main_road_skin, skins, road_skins = args
 
     settings_file['song'] = song
     saving['coins'] = coins
     saving['car_skin'] = main_skin
-    saving['road_skin'] = road_skin
+    saving['road_skin'] = main_road_skin
     saving['car_skins'] = skins
+    saving['road_skins'] = road_skins
 
     with open('saving.json', 'w') as f:
         json.dump(saving, f)
@@ -57,7 +61,7 @@ def close(*args):
 
 def play():
     global main_skin
-    global road_skin
+    global main_road_skin
 
     timer = Timer(-1, screen, size)
     timer.seconds = 4
@@ -72,9 +76,10 @@ def play():
     npc_particles = pygame.sprite.Group()
     overlay_group = pygame.sprite.Group()
 
-    road = Road(load_image(road_skins[road_skin]), sprites)
+    road = Road(load_image(list(road_skins.keys())[main_road_skin]), sprites)
 
-    car = Car(load_image(f'car_{list(skins.keys())[main_skin]}_up'), road, npc_cars, player)
+    car = Car(load_image(f'car_{list(skins.keys())[main_skin]}_up'), road,
+              npc_cars, player)
 
     overlay = UiSprite(load_image('overlay'), (0, 0), overlay_group)
 
@@ -109,7 +114,8 @@ def play():
                              load_image('buttons/continue_hl'), (396, 560),
                              pause_menu_sprites)
 
-    warning = Text(screen, size, 15, "Score won't be saved if you leave!", 'white')
+    warning = Text(screen, size, 15, "Score won't be saved if you leave!",
+                   'white')
     warning.dest = ((size[0] - warning.render.get_width()) // 2, 530)
 
     timer_sfx = pygame.mixer.Sound('assets/sfx/Countdown.mp3')
@@ -120,7 +126,7 @@ def play():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 close(current_song, coins_count.coins_count, main_skin,
-                      road_skin, skins)
+                      main_road_skin, skins, road_skins)
 
             if paused:
                 if home_button.update(event):
@@ -256,7 +262,7 @@ def play():
 
 def main_menu():
     global car_skin
-    global road_skin
+    global main_road_skin
     global skins
     global main_skin
 
@@ -269,9 +275,10 @@ def main_menu():
 
     label = Text(screen, size, 50, 'Menu', 'white')
     label.dest = (10, 10)
-    road = Road(load_image(road_skins[road_skin]), sprites)
+    road = Road(load_image(list(road_skins.keys())[main_road_skin]), sprites)
 
-    car = Car(load_image(f'car_{list(skins.keys())[main_skin]}_up'), road, npc_cars, player)
+    car = Car(load_image(f'car_{list(skins.keys())[main_skin]}_up'), road,
+              npc_cars, player)
     overlay = UiSprite(load_image('overlay'), (0, 0), overlay_group)
 
     play_button = Button(load_image('buttons/play'),
@@ -302,15 +309,18 @@ def main_menu():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 close(current_song, coins_count.coins_count, main_skin,
-                      road_skin, skins)
+                      main_road_skin, skins, road_skins)
             if play_button.update(event):
                 play()
             if skins[list(skins.keys())[car_skin]][0] == 'locked':
                 if lock_button.update(event):
-                    if coins_count.coins_count - skins[list(skins.keys())[car_skin]][1] >= 0:
+                    if coins_count.coins_count - \
+                            skins[list(skins.keys())[car_skin]][1] >= 0:
                         skins[list(skins.keys())[car_skin]][0] = 'open'
                         main_skin = car_skin
-                        coins_count.coins_count = coins_count.coins_count - skins[list(skins.keys())[car_skin]][1]
+                        coins_count.coins_count = coins_count.coins_count - \
+                                                  skins[list(skins.keys())[
+                                                      car_skin]][1]
             if arrow_left.update(event):
                 if car_skin - 1 < 0:
                     car_skin = len(skins) - 1
@@ -324,7 +334,7 @@ def main_menu():
                 skin_info_msg = f'{skins[list(skins.keys())[car_skin]][0].capitalize()}. Price: {skins[list(skins.keys())[car_skin]][1]}'
                 skin_info.set_text(skin_info_msg)
                 skin_info.dest = (
-                (size[0] - skin_info.render.get_width()) // 2, 350)
+                    (size[0] - skin_info.render.get_width()) // 2, 350)
             if arrow_right.update(event):
                 if car_skin + 1 > len(skins) - 1:
                     car_skin = 0
@@ -338,7 +348,7 @@ def main_menu():
                 skin_info_msg = f'{skins[list(skins.keys())[car_skin]][0].capitalize()}. Price: {skins[list(skins.keys())[car_skin]][1]}'
                 skin_info.set_text(skin_info_msg)
                 skin_info.dest = (
-                (size[0] - skin_info.render.get_width()) // 2, 350)
+                    (size[0] - skin_info.render.get_width()) // 2, 350)
             if settings_button.update(event):
                 settings()
             if road_button.update(event):
@@ -367,7 +377,7 @@ def main_menu():
 
 def settings():
     global car_skin
-    global road_skin
+    global main_road_skin
     global current_song
 
     menu_sprites = pygame.sprite.Group()
@@ -378,7 +388,7 @@ def settings():
 
     label = Text(screen, size, 50, 'Settings', 'white')
     label.dest = ((size[0] - label.render.get_width()) // 2, 10)
-    road = Road(load_image(road_skins[road_skin]), sprites)
+    road = Road(load_image(list(road_skins.keys())[main_road_skin]), sprites)
 
     current_song_name = music[current_song]
     rendering_song_name = current_song_name if len(
@@ -387,7 +397,8 @@ def settings():
     song_name.dest = ((size[0] - song_name.render.get_width()) // 2,
                       100 + 30 - song_name.render.get_height() // 2)
 
-    car = Car(load_image(f'car_{list(skins.keys())[main_skin]}_up'), road, npc_cars, player)
+    car = Car(load_image(f'car_{list(skins.keys())[main_skin]}_up'), road,
+              npc_cars, player)
     overlay = UiSprite(load_image('overlay'), (0, 0), overlay_group)
 
     song_section = Text(screen, size, 20, 'Music', 'white')
@@ -409,7 +420,7 @@ def settings():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 close(current_song, coins_count.coins_count, main_skin,
-                      road_skin, skins)
+                      main_road_skin, skins, road_skins)
             if arrow_left.update(event):
                 if current_song - 1 < 0:
                     current_song = len(music) - 1
@@ -454,17 +465,22 @@ def settings():
 
 def road_menu():
     global car_skin
+    global main_road_skin
     global road_skin
 
     menu_sprites = pygame.sprite.Group()
     sprites = pygame.sprite.Group()
     npc_cars = pygame.sprite.Group()
     player = pygame.sprite.Group()
+    lock_button_group = pygame.sprite.Group()
+
+    road_skin = main_road_skin
 
     label = Text(screen, size, 50, 'Road', 'white')
     label.dest = (10, 10)
-    road = Road(load_image(road_skins[road_skin]), sprites)
-    car = Car(load_image(f'car_{list(skins.keys())[main_skin]}_up'), road, npc_cars, player)
+    road = Road(load_image(list(road_skins.keys())[main_road_skin]), sprites)
+    car = Car(load_image(f'car_{list(skins.keys())[main_skin]}_up'), road,
+              npc_cars, player)
 
     arrow_right = Button(load_image('buttons/arrow'),
                          load_image('buttons/arrow_hl'), (340, 420),
@@ -478,11 +494,28 @@ def road_menu():
                          load_image('buttons/back_hl'), (12, 560),
                          menu_sprites)
 
+    lock_button = Button(load_image('buttons/lock'),
+                         load_image('buttons/lock_hl'), (216, 415),
+                         lock_button_group)
+
+    skin_info_msg = f'{road_skins[list(road_skins.keys())[road_skin]][0].capitalize()}. Price: {road_skins[list(road_skins.keys())[road_skin]][1]}'
+    skin_info = Text(screen, size, 20, skin_info_msg, 'Black')
+    skin_info.dest = ((size[0] - skin_info.render.get_width()) // 2, 350)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 close(current_song, coins_count.coins_count, main_skin,
-                      road_skin, skins)
+                      main_road_skin, skins, road_skins)
+            if road_skins[list(road_skins.keys())[road_skin]][0] == 'locked':
+                if lock_button.update(event):
+                    if coins_count.coins_count - \
+                            road_skins[list(road_skins.keys())[road_skin]][1] >= 0:
+                        road_skins[list(road_skins.keys())[road_skin]][0] = 'open'
+                        main_road_skin = road_skin
+                        coins_count.coins_count = coins_count.coins_count - \
+                                                  road_skins[list(road_skins.keys())[
+                                                      road_skin]][1]
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     main_menu()
@@ -491,13 +524,32 @@ def road_menu():
                     road_skin = len(road_skins) - 1
                 else:
                     road_skin -= 1
-                change_road(road, road_skins[road_skin])
+                change_road(road, list(road_skins.keys())[road_skin])
+
+                if road_skins[list(road_skins.keys())[road_skin]][0] == 'open':
+                    main_road_skin = road_skin
+                    print(main_road_skin)
+
+                skin_info_msg = f'{road_skins[list(road_skins.keys())[road_skin]][0].capitalize()}. Price: {road_skins[list(road_skins.keys())[road_skin]][1]}'
+                skin_info.set_text(skin_info_msg)
+                skin_info.dest = (
+                    (size[0] - skin_info.render.get_width()) // 2, 350)
+
             if arrow_right.update(event):
                 if road_skin + 1 > len(road_skins) - 1:
                     road_skin = 0
                 else:
                     road_skin += 1
-                change_road(road, road_skins[road_skin])
+                change_road(road, list(road_skins.keys())[road_skin])
+
+                if road_skins[list(road_skins.keys())[road_skin]][0] == 'open':
+                    main_road_skin = road_skin
+                    print(main_road_skin)
+
+                skin_info_msg = f'{road_skins[list(road_skins.keys())[road_skin]][0].capitalize()}. Price: {road_skins[list(road_skins.keys())[road_skin]][1]}'
+                skin_info.set_text(skin_info_msg)
+                skin_info.dest = (
+                    (size[0] - skin_info.render.get_width()) // 2, 350)
 
             if back_button.update(event):
                 main_menu()
@@ -514,6 +566,10 @@ def road_menu():
 
         label.update()
         coins_count.update()
+
+        if road_skins[list(road_skins.keys())[road_skin]][0] == 'locked':
+            skin_info.update()
+            lock_button_group.draw(screen)
 
         clock.tick(fps)
         pygame.display.update()
@@ -539,7 +595,9 @@ if __name__ == '__main__':
     car_skin = saving['car_skin']
     main_skin = car_skin
     road_skin = saving['road_skin']
+    main_road_skin = road_skin
     skins = saving['car_skins']
+    road_skins = saving['road_skins']
 
     coins_count = CoinsCount(screen, size, 40, '#f7e26b')
     coins_count.coins_count = saving['coins']
@@ -548,4 +606,5 @@ if __name__ == '__main__':
     set_music(music[current_song])
 
     main_menu()
-    close(current_song, coins_count.coins_count, main_skin, road_skin, skins)
+    close(current_song, coins_count.coins_count, main_skin,
+          main_road_skin, skins, road_skins)
