21f1ad7e25476ace3570eb7d46a1b61800116e1b
pavlen5kiy <wolkpavel51@gmail.com>
2024-08-16 18:27:01 +0300

    Coins spawn system modified

--- coins_controller.py
+++ coins_controller.py
@@ -13,7 +13,13 @@ def set_coin(x_pos, car, road, coins_count, sprites):
 
 def spawn_coins(car, road, coins_count, sprites):
     for i in range(4):
-        if random.randrange(1, 10) == 9:
+        spawn = False
+        attempts = 1 + road.score // 50 - road.score // 100
+        for j in range(attempts):
+            if random.randrange(1, 10) == 9:
+                spawn = True
+                break
+        if spawn:
             set_coin(60 + 100 * i, car, road, coins_count, sprites)
         else:
             continue
